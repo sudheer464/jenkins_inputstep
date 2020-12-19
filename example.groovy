c4370c77f7df20a6118616b7e2baebd4f5668b19
sudheer464 <sudheer464@users.noreply.github.com>
2020-12-19 15:56:09 -0500

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -1,9 +1,9 @@
 def example1() {
-  println 'Hello from example1'
+  println 'Hello from example1-test'
 }
 
 def example2() {
-  println 'Hello from example2'
+  println 'Hello from example2-test'
 }
 
 return this
